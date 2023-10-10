596f7fb17177937b09cae35c1f5003e3ddc4ee01
Pavithrag0103 <pavithrag0103@gmail.com>
2023-10-10 14:40:04 +0530

    commit good

--- good.py
+++ good.py
@@ -22,3 +22,17 @@ for j in range(1, rows + 1):
     print("* " * j)
 
 
+a=[2,5,3,4]
+a[2:2]=[2]
+print(a)
+
+my_dict={"a":1,"b": 2,"c":3}
+result=my_dict.values()
+print(result)
+
+
+i=0
+while i<3:  #0  1   2
+    print(i)    #0  1   2    (0,2,1,3,2,4)
+    i=i+1    #1   2   3
+    print(i+1)  #2   3   4
