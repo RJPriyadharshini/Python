d5ed5a680661b2cd3458a32a0cc6e1441d9f021a
Pavithrag0103 <pavithrag0103@gmail.com>
2023-10-23 18:26:49 +0530

    Commited

--- Guvi.py
+++ Guvi.py
@@ -1,3 +1,21 @@
+import datetime
+
+def fun():
+    f = open("guvi.txt", "w")
+    print(f.name)
+    if f.name == "guvi.txt":
+        x = datetime.datetime.now()
+        f.write(f'Current date and time: " + {x}')
+        f.close()  # Don't forget to close the file after writing
+        print("Current date and time written to the file.")
+    else:
+        print("Error")
+
+fun()
+
+
+
+
 """Shape	Area	Perimeter
 Circle	A = π × r2	Circumference = 2πr"""
 
